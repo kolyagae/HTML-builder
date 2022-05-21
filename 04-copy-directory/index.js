1b387d40c13f1861b5175df7d8b26dc4f853bc92
EVGEN <k.e.l@tut.by>
2022-05-21 14:03:32 +0300

    complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+  if (err) {throw err;}
+});
+
+fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+  if (err) {
+    throw err;
+  } else {
+    for (let file of files) {
+      fs.unlink(path.join(__dirname, 'files-copy', file), (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+  }
+}
+);
+
+async function makeCopy () {
+  await fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) {
+      throw err;
+    } 
+    else {
+      for (let file of files) {
+        fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+    }
+  });
+}
+makeCopy();
